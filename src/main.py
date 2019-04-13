61b7aac0974e3b0bba7193e78d9871542183f234
Danny McAllaster <dmcallas@gmail.com>
2019-04-12 23:55:31 -0700

    Construct chunks from noweb file

--- src/main.py
+++ src/main.py
@@ -1,3 +1,91 @@
+import re
+import os
+import sys
 
-def some_fn(a, b):
-    return abs(a+b)
+def file_to_string(file_name):
+    with open(file_name, 'r') as f:
+        data = f.read()
+    return data
+
+
+def write_to_file(file_name, content):
+    with open(file_name, 'w') as f:
+        f.write(content)
+
+
+class ChunkParser:
+    def __init__(self, text, linesep=os.linesep):
+        self._text = text
+        self._extract_chunks(text)
+        self._nl = linesep
+
+
+    def _extract_chunks(self, input_text: str):
+        in_chunk = False
+        chunk_name = ''
+        chunk = ''
+        self.chunks = dict()
+        self.deps = dict()
+        matcher = re.compile(r"<<(?P<chunk_name>.*)>>=")
+        include_chunk_matcher = re.compile(r"<<(?P<chunk_name>.*?)>>")
+        for line in input_text.splitlines():
+            if not in_chunk:
+                match = matcher.match(line)
+                if match:
+                    in_chunk = True
+                    chunk_name = match.group('chunk_name')
+                    chunk = []
+                    self.deps[chunk_name] = set()
+            else:
+                if line == '@':
+                    in_chunk = False
+                    self.chunks[chunk_name] = chunk
+                    chunk = ''
+                    chunk_name = ''
+                else:
+                    included_chunks = include_chunk_matcher.findall(line)
+                    self.deps[chunk_name].update(included_chunks)
+                    chunk.append(line)
+
+    def get_chunk_deps(self, chunk: str):
+        return self.deps[chunk]
+
+    def get_chunk_names(self):
+        return self.chunks.keys()
+
+    def __contains__(self, item: str):
+        return item in self.chunks
+
+    def fetch_chunk_lines(self, chunk_name: str):
+        chunk_lines = self.chunks.get(chunk_name,"")
+        processed_lines = []
+        for line in chunk_lines:
+            processed = False
+            for dep in self.get_chunk_deps(chunk_name):
+                # Don't process if it isn't actually a chunk name:
+                dep_str = f"<<{dep}>>"
+                if dep_str in line and dep in self and not processed:
+                    rep_chunk_lines = self.fetch_chunk_lines(dep)
+                    split = line.split(dep_str)
+                    for line in rep_chunk_lines:
+                        processed_lines.append(line.join(split))
+                    processed = True
+            if not processed:
+                processed_lines.append(line)
+        return processed_lines
+
+
+    def fetch_chunk(self, chunk_name: str):
+        chunk_lines = self.fetch_chunk_lines(chunk_name)
+        return self._nl.join(chunk_lines)+self._nl
+
+    def __getitem__(self, key: str):
+        return self.fetch_chunk(key)
+
+
+if __name__ == '__main__':
+    file_str = file_to_string(sys.argv[1])
+    chunks = ChunkParser(file_str)
+    for chunk in chunks.get_chunk_names():
+        if chunk.startswith('file:'):
+            write_to_file(chunk[5:],chunks[chunk])
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,155 @@
-from main import some_fn
+from main import ChunkParser
 
-def test_point_mul():
-    x = 1
-    y = 2
-    assert 3 == some_fn(x, y)
+def test_extract_chunks():
+    test_string = """
+<<test1>>=
+asdf
+@
 
-    assert 1 == some_fn(1, -2)
+some text
+
+<<test2>>=
+qwerty
+ zxcv
+@
+    """
+    chunks = ChunkParser(test_string)
+    assert chunks.fetch_chunk("test1") == "asdf\n"
+    assert chunks["test2"] == "qwerty\n zxcv\n"
+
+def test_only_at_terminates_chunks():
+    test_string = """
+<<test1>>=
+asdf
+
+some text
+
+<<test2>>=
+qwerty
+ zxcv
+@
+    """
+    chunks = ChunkParser(test_string)
+    assert "test1" in chunks
+    assert chunks["test1"] == "asdf\n\nsome text\n\n<<test2>>=\nqwerty\n zxcv\n"
+    assert "test2" not in chunks
+    assert "test2" not in chunks.get_chunk_names()
+
+
+def test_with_at_in_chunk():
+    test_string = """
+<<test1>>=
+asdf@1234
+@
+    """
+    chunks = ChunkParser(test_string)
+    assert chunks["test1"] == "asdf@1234\n"
+
+def test_construct_chunk():
+    test_string = """
+<<test1>>=
+foo
+bar
+@
+<<test2>>=
+qwerty
+<<test1>>
+asdfgh
+@
+    """
+    chunks = ChunkParser(test_string)
+    assert chunks["test1"] == "foo\nbar\n"
+    assert chunks["test2"] == "qwerty\nfoo\nbar\nasdfgh\n"
+
+def test_construct_chunk_preserves_indent():
+    test_string = """
+<<test1>>=
+  foo
+  bar
+@
+<<test2>>=
+qwerty
+<<test1>>
+asdfgh
+@
+    """
+    chunks = ChunkParser(test_string)
+    assert chunks["test1"] == "  foo\n  bar\n"
+    assert chunks["test2"] == "qwerty\n  foo\n  bar\nasdfgh\n"
+
+
+def test_construct_chunk_adds_indent():
+    test_string = """
+<<test1>>=
+foo
+bar
+@
+<<test2>>=
+qwerty
+  <<test1>>
+asdfgh
+@
+    """
+    chunks = ChunkParser(test_string)
+    assert chunks["test1"] == "foo\nbar\n"
+    assert chunks["test2"] == "qwerty\n  foo\n  bar\nasdfgh\n"
+
+
+def test_construct_chunk_combines_indent():
+    test_string = """
+<<test1>>=
+  foo
+  bar
+@
+<<test2>>=
+qwerty
+  <<test1>>
+asdfgh
+@
+    """
+    chunks = ChunkParser(test_string)
+    assert chunks["test1"] == "  foo\n  bar\n"
+    assert chunks["test2"] == "qwerty\n    foo\n    bar\nasdfgh\n"
+
+def test_construct_chunk_with_multiple_children_on_line():
+    test_string = """
+<<test1>>=
+foo
+bar
+@
+<<test2>>=
+baz
+@
+<<test3>>=
+qwerty
+<<test1>>
+<<test2>>
+asdfgh
+@
+    """
+    chunks = ChunkParser(test_string)
+    assert chunks["test1"] == "foo\nbar\n"
+    assert chunks["test2"] == "baz\n"
+    assert chunks["test3"] == "qwerty\nfoo\nbar\nbaz\nasdfgh\n"
+
+def test_circular_include():
+    test_string = """
+<<test1>>=
+<<test2>>
+foo
+@
+<<test2>>=
+<<test1>>
+bar
+@
+    """
+    
+    chunks = ChunkParser(test_string)
+    try:
+        chunks["test1"]
+        assert False, "Should've thrown an exception"
+    except RecursionError:
+        pass
+        
+
 
